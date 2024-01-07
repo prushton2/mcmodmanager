2fbe726ce5d77b07218c747a6ff79f71974ae4b7
prushton2 <peterrushton418@gmail.com>
2024-01-06 19:43:13 -0500

    Added a basic UI and config

---
+++ src/file.rs
@@ -0,0 +1,57 @@
+use serde_json;
+use serde::{Serialize, Deserialize};
+use std::fs;
+use std::env;
+use std::io::Write;
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+	pub os: String
+}
+
+
+pub fn load_config(file_name: &str) -> Result<Config, &str> {
+	let file_contents = fs::read_to_string(file_name);
+
+
+	if file_contents.is_err() {
+		return Err("File could not be read")
+	}
+
+	let config: Config = serde_json::from_str(&file_contents.unwrap()).unwrap();
+
+	return Ok(config)
+}
+
+
+pub fn write_config(file_name: &str, config: Config) -> Result<&str, &str> {
+	
+	let file_result = fs::File::create(file_name);
+	let serialized = serde_json::to_string(&config);
+
+	if serialized.is_err() {
+		return Err("Serialization Failed")
+	}
+
+	if file_result.is_err() {
+		return Err("Error opening file")
+	}
+
+	let mut file = file_result.unwrap();
+
+	let _ = file.write_all(serialized.unwrap().as_bytes());
+	return Ok("File Written")
+}
+
+
+
+pub fn delete_file(file_name: &str) -> Result<&str, &str> {
+	let result = fs::remove_file(file_name);
+
+	if result.is_ok() {
+		return Ok("File Removed");
+	} else {
+		return Err("Error occurred during file removal");
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,97 @@
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
-use egui::Ui;
+use iced::widget::{container, text, button, column, row};
+use iced::{Element, Sandbox, Settings};
 
-fn main() -> Result<(), eframe::Error> {
+mod file;
+
+static FILE_PATH: &str = "./config";
+
+
+fn main() -> iced::Result {
     
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-    
-    eframe::run_native(
-        "MC Mod Manager",
-        options,
-        Box::new(|_cc| Box::<UserInterface>::default()),
-    )
-
+    ModLoader::run(Settings::default())
 }
 
-#[derive(Default)]
-struct UserInterface {
-    state: i64,
-    // show_confirmation_dialog: bool,
-    // allowed_to_close: bool,
+
+struct ModLoader {
+    page: i64,
+    os: String,
 }
 
-impl eframe::App for UserInterface {
+#[derive(Debug, Clone)]
+enum Message {
+    Next,
+    Previous,
+    OsSetWindows,
+    OsSetLinux
+}
 
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
-        egui::CentralPanel::default().show(ctx, |ui| {
-        
-            match self.state {
-                10 => {
-                    self.state = 0;
-                },
-                _ => {}
-            }
-            
-            // ui.heading(format!("State is {}", self.state));
-            
-            main_menu(ui, self);
-        
-            
-            ui.with_layout(egui::Layout::right_to_left(egui::Align::BOTTOM), |ui| {
-                ui.horizontal(|ui| {
-                    if ui.button("Next").clicked() {
-                        self.state += 1;
-                    }
-                    if ui.button("Back").clicked() {
-                        self.state -= 1;
-                    }
-                    
-                })
+impl Sandbox for ModLoader {
+    type Message = Message;
 
-            })
+    fn new() -> Self {
 
-            
-            // ui.heading("Try to close the window");
-            
-        });
-    
+        let load_result = file::load_config(FILE_PATH);
+        let mut config: file::Config;
+
+        if load_result.is_ok() {
+            config = load_result.unwrap();
+        } else {
+            config = file::Config {
+                os: String::from("Windows"),
+            };
+        }
+
+        return Self {
+            page: 0,
+            os: config.os
+        }
     }
+
+    fn title(&self) -> String {
+        return String::from("MC Mod Loader")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::Next => self.page += 1,
+            Message::Previous => self.page -= 1,
+            Message::OsSetLinux => {
+                self.os = String::from("linux");
+                save_state(&self);
+            },
+            Message::OsSetWindows => {
+                self.os = String::from("windows");
+                save_state(&self);
+            },
+        };
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        
+        let next = button("Back").on_press(Message::Previous);
+        let prev = button("Next").on_press(Message::Next);
+        
+
+
+        
+        let element = column![
+            text(format!("OS: {}", self.os)),
+            row![button("windows").on_press(Message::OsSetWindows), button("linux").on_press(Message::OsSetLinux)],
+            row![next, prev]
+        ];
+
+        return container(element).into()
+        
+    }
+
 }
 
-fn main_menu(ui: &mut Ui, state: &mut UserInterface) {
-    ui.heading(format!("State is {}", state.state));
+fn save_state(this: &ModLoader) -> Result<&str, &str> {
+    let config: file::Config = file::Config {
+        os: this.os.clone()
+    };
+
+    return file::write_config(FILE_PATH, config)
 }
